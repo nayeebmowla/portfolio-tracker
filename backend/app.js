893552b55f8348e931cdc151939e4b1fc3919475
Nayeeb <nayeebmowla@github.com>
2023-05-28 12:16:06 -0400

    fix backend

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,13 @@
 const express = require('express');
-// const hbs = require('hbs');
 const fetchPositions = require('./utils/questrade');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.set('view engine', 'hbs');
 app.use(express.json());
-// app.use(express.static('public'));
 
-// hbs.registerHelper('range', function (count) {
-//   return Array.from({ length: count }, (_, i) => i + 1);
-// });
-
-// hbs.registerHelper('formatCurrency', function(value) {
-//   const roundedValue = Number(value).toFixed(2);
-//   const formattedValue = `$${roundedValue}`;
-//   return new hbs.SafeString(formattedValue);
-// });
-
-app.get('/', async (req, res) => {
+app.get('/positions', async (req, res) => {
   const token = req.query.token;
   if(!token) {
     return res.send({
@@ -29,12 +16,11 @@ app.get('/', async (req, res) => {
   }
 
   try {
-    const data = await fetchPositions(token);
-    const positions = data.filter(stock => stock.symbol.includes('.TO')).sort((a, b) => a.symbol.localeCompare(b.symbol));
-    res.render('index', { positions });
+    const positions = await fetchPositions(token);
+    res.json(positions);
   } catch (e) {
     console.log(e);
-    res.status(500).send(e);
+    res.status(400).json("Token may be expired, please try with a new token.\nError: " + e);
   }
 });
 
