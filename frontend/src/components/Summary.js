7a5dc62780a68034f1f1106352c1112160f8094e
Nayeeb <nayeebmowla@github.com>
2023-09-24 03:55:27 -0400

    init pie chart

--- frontend/src/components/Summary.js
+++ frontend/src/components/Summary.js
@@ -1,8 +1,30 @@
 import React from "react";
 import PortfolioSelect from "./PortfolioSelect";
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
+import { PieChart, pieArcLabelClasses } from "@mui/x-charts/PieChart";
+import { useTheme } from "@mui/material/styles";
+
+const data = [
+  { label: "Group A", value: 400 },
+  { label: "Group B", value: 300 },
+  { label: "Group C", value: 300 },
+  { label: "Group D", value: 200 },
+];
+
+const sizing = {
+  width: 400,
+  height: 200,
+};
+const TOTAL = data.map((item) => item.value).reduce((a, b) => a + b, 0);
+
+const getArcLabel = (params) => {
+  const percent = params.value / TOTAL;
+  return `${(percent * 100).toFixed(0)}%`;
+};
 
 function Summary({ account, onAccountSelect, accounts }) {
+  const theme = useTheme();
+
   return (
     <Stack spacing={2}>
       <PortfolioSelect
@@ -10,7 +32,27 @@ function Summary({ account, onAccountSelect, accounts }) {
         account={account}
         onChange={onAccountSelect}
       />
-      <div>Summary...</div>
+      <br />
+      <Typography color={theme.text.headers} variant="h5" fontWeight="bold">
+        {`${account}`}
+      </Typography>
+      <PieChart
+        series={[
+          {
+            outerRadius: 80,
+            data,
+            arcLabel: getArcLabel,
+            arcLabelMinAngle: 45,
+          },
+        ]}
+        sx={{
+          [`& .${pieArcLabelClasses.root}`]: {
+            fill: "white",
+            fontSize: 14,
+          },
+        }}
+        {...sizing}
+      />
     </Stack>
   );
 }
